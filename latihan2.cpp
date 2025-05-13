8472b16c156b0120ef1894fb069f79de499b4f9b
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-05-13 10:35:32 +0700

    mendefinisikan constructor dan membuat object

--- latihan2.cpp
+++ latihan2.cpp
@@ -6,3 +6,12 @@ public:
 mahasiswa();
 };
 
+mahasiswa::mahasiswa(){
+cout <<"Constructor Terpanggil"<<endl; //definisi constructor
+}
+
+int main(){
+mahasiswa mhs; //pembuatan object
+
+return 0;
+}
