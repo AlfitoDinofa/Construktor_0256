d03a248d7858283734aa07f5bafdbd4fae8ec268
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-05-13 11:09:48 +0700

    menampilkan integer utama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -30,3 +30,19 @@ nim=iNim;
 nama=iNama;
 }
 
+void mahasiswa::cetak(){
+cout<<endl<<"Nim = " <<nim<<endl;
+cout<<"Nama = " <<nama<<endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Fito");
+    mahasiswa mhs3(102030, "Fito");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
