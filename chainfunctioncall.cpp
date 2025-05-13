864c3c4e78f755bda42af28a09a6cde946ebfcaa
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-05-13 11:22:45 +0700

    chain function call

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class buku{
+    string judul;
+
+    public:
+    buku setjudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getjudul(){
+        return this->judul;
+    }
+
+};
+
+int main()
+{
+    buku bukunya;
+    cout <<bukunya.setjudul("Matematika").getjudul();
+    return 0;
+}
