7647ba5531e2f4a000e51a4c64e8d8c43f78545d
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-05-13 10:39:18 +0700

    definisi constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,12 @@ public:
     mahasiswa(int nim, string nama);
 };
 
+mahasiswa::mahasiswa(int nim, string nama) { //definisi constructor dengan parameter
+    cout << "constructor dengan parameter Terpanggil" << endl;
+    cout << "NIM  : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+int main() {
+    mahasiswa mhs(12345, "Fito");
+    return 0;
+}
